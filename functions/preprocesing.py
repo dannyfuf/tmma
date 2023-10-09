dbc71ff93f246898d3fede6ea92ce9010baa5481
Danny Fuentes <dannyfuf@gmail.com>
2023-10-09 03:07:21 -0300

    created method to get the minimum buffer radius using naive aproach

---
+++ functions/preprocesing.py
@@ -0,0 +1,39 @@
+from qgis.core import *
+import qgis.utils
+
+from qgis_utils import get_layer_by_name, get_object_count_in_layer, create_point, create_buffer
+
+def tune_buffer_radius(
+    roads_layer_name,
+    gps_layer_name,
+    radius_range=[1, 10],
+    radius_step=[2]
+):
+    print('-----------------------------------------')
+    print('starting tuning buffer radius')
+    print('roads_layer_name: ', roads_layer_name)
+    print('gps_layer_name: ', gps_layer_name)
+    print('radius_range: ', radius_range)
+    print('radius_step: ', radius_step)
+    print('-----------------------------------------')
+    points_layer = get_layer_by_name(gps_layer_name)
+    len_points_layer = get_object_count_in_layer(points_layer)
+
+    for radius in range(radius_range[0], radius_range[1]+1, radius_step):
+        print('testing radius: ', radius)
+        points_within_road = 0
+
+        for point in points_layer.getFeatures():
+            point = create_point(point)
+            buffer = create_buffer(point, radius)
+            roads_layer = get_layer_by_name(roads_layer_name)
+
+            for feature in roads_layer.getFeatures():
+                road = feature.geometry()
+                if road.intersects(buffer):
+                    points_within_road += 1
+                    break
+
+            if points_within_road / len_points_layer >= 0.99:
+                return radius
+    return radius
