5d76dbb19ce6ac15a18c40251fb0fc9f6613473f
Danny Fuentes <dannyfuf@gmail.com>
2023-10-22 00:36:13 -0300

    methods to work with points

---
+++ functions/points.py
@@ -0,0 +1,25 @@
+from qgis.core import *
+
+# ------------------------------------------------------------
+#                         BUILDERS
+# ------------------------------------------------------------
+
+def create_point(
+    object: QgsFeature,
+    x_name='X',
+    y_name='Y'
+):
+    x, y = object[x_name], object[y_name]
+    point_xy = QgsPointXY(x, y)
+    return QgsGeometry.fromPointXY(point_xy)
+
+def create_buffer(
+    point: QgsGeometry,
+    radius: float,
+    segments=5
+):
+    return point.buffer(radius, segments)
+
+# ------------------------------------------------------------
+#                         GETTERS
+# ------------------------------------------------------------
