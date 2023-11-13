b5e1f0a565615a81d6cbc91dae4b70a43c57ba2f
Danny Fuentes <dannyfuf@gmail.com>
2023-11-13 00:30:03 -0300

    add method to get projection of point to line

--- geometry/lines.py
+++ geometry/lines.py
@@ -4,3 +4,11 @@ def create_line(
     object: QgsFeature
 ):
     return object.geometry()
+
+
+def get_point_projection_on_line(
+    point: QgsFeature,
+    line: QgsFeature
+):
+    point = line.geometry().closestSegmentWithContext(point.geometry().asPoint())[1]
+    return QgsGeometry.fromPointXY(point)
