6dac29aca0b2a727ce9f33412586da7125b14884
Danny Fuentes <dannyfuf@gmail.com>
2023-11-22 01:43:31 -0300

    return modified point with original data but position chenged to the projected

--- geometry/lines.py
+++ geometry/lines.py
@@ -1,5 +1,9 @@
 from qgis.core import *
 
+from PyQt5.QtCore import QVariant
+
+from geometry.layers import copy_feature
+
 def create_line(
     object: QgsFeature
 ):
@@ -10,5 +14,9 @@ def get_point_projection_on_line(
     point: QgsFeature,
     line: QgsFeature
 ):
-    point = line.geometry().closestSegmentWithContext(point.geometry().asPoint())[1]
-    return QgsGeometry.fromPointXY(point)
+    projected_point = line.geometry().closestSegmentWithContext(point.geometry().asPoint())[1]
+    x, y = projected_point.x(), projected_point.y()
+    new_point = copy_feature(point)
+    new_point.setAttribute('X', x)
+    new_point.setAttribute('Y', y)
+    return new_point
