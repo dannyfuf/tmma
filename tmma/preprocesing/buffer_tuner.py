8d830b245651247d258a3e1260eb12061fd32455
Danny Fuentes <dannyfuf@gmail.com>
2024-02-24 21:11:38 -0300

    feat: add class to get the buffer where all points have at least one candidate

---
+++ tmma/preprocesing/buffer_tuner.py
@@ -0,0 +1,18 @@
+from ..distance_index.distance_index import DistanceIndex
+
+class BufferTuner:
+    distance_index: DistanceIndex
+
+    def __init__(self, distance_index: DistanceIndex):
+        self.distance_index = distance_index
+
+    def get_buffer(self):
+        buffer_size = 0
+
+        for point_id in self.distance_index.distances():
+            distance_to_nearest_road = self.distance_index.distances()[point_id].distances_queue[0].distance
+
+            if distance_to_nearest_road > buffer_size:
+                buffer_size = distance_to_nearest_road
+        
+        return buffer_size
