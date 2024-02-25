639eaee9f0168a4dd948acfc08e6d401e25e923f
Danny Fuentes <dannyfuf@gmail.com>
2024-02-25 00:12:27 -0300

    chore: remove unused definition

--- gis/layer/types/normalizer.py
+++
@@ -1,8 +0,0 @@
-from typing import TypedDict, Literal
-from qgis.core import QgsVectorLayer, QgsProject, QgsCoordinateReferenceSystem
-
-class NormalizeCRSParams(TypedDict):
-    crs_from: QgsCoordinateReferenceSystem
-    context: QgsProject
-
-LayerType = Literal['MultiLineString', 'MultiPoint']
