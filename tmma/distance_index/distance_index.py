18178ebb462f34c8c459ba7cb56f68b40fee0d81
Danny Fuentes <dannyfuf@gmail.com>
2024-04-18 23:02:45 -0400

    chore: remove logic to keep state in tmma

--- tmma/distance_index/distance_index.py
+++ tmma/distance_index/distance_index.py
@@ -10,15 +10,10 @@ class DistanceIndex:
     __road_layer: Layer
     __gps_layer: Layer
     __points_order: list[str]
-    __current_point: DistanceIndexElement
-    __current_candidate_road: QgsFeature
 
     def __init__(self, road_layer: Layer, gps_layer: Layer, distance_index: dict[str, list[tuple[str, float]]] = None):
         self.__road_layer = road_layer
         self.__gps_layer = gps_layer
-        self.__points_order = self.__gps_layer.points_order()
-        self.__current_point = None
-        self.__current_candidate_road = None
         if distance_index:
             self.__distances = self.__build_distance_elements(distance_index)
         else:
@@ -46,51 +41,12 @@ class DistanceIndex:
         project.save_distance_index(tmp_distance_index)
         distance_index = self.__build_distance_elements(tmp_distance_index)
         return distance_index
-
-
-    def current_point(self):
-        query = f"fid = {self.__current_point.point_id}"
-        return self.__gps_layer.query(query)[0]
-    
-    def current_point_id(self):
-        return self.__current_point.point_id
-
-    def set_next_point(self):
-        if len(self.__points_order) == 0:
-            self.__current_point = None
-            return False
-
-        idx = str(self.__points_order.pop(0))
-        self.__current_point = self.__distances[idx]
-        return True
-
-    def set_next_candidate_road(self):
-        if self.__current_point is None:
-            return False
-
-        fid_road = self.__current_point.get_next_candidate_road_id()
-        if fid_road is None:
-            self.__current_candidate_road = None
-            return False
-        self.__current_candidate_road = self.__road_layer.query(f"fid = {fid_road}")[0]
-        return True
-    
-    def get_next_candidate_road(self):
-        self.set_next_candidate_road()
-        return self.__current_candidate_road
-    
-    def road_candidate_id(self):
-        if self.__current_candidate_road is None:
-            return None
-        return self.__current_candidate_road['fid']
-    
-    def current_point_id(self):
-        if self.__current_point is None:
-            return None
-        return self.__current_point.point_id
     
     def distances(self):
         return self.__distances
+
+    def get_distances_from_point(self, point_id):
+        return self.__distances[point_id].distances_queue
 
     def remove_roads_outside_buffer(self, buffer):
         for point_id in self.distances():
--- tmma/distance_index/distance_index_element.py
+++ tmma/distance_index/distance_index_element.py
@@ -3,7 +3,6 @@ from .distance import Distance
 class DistanceIndexElement:
     point_id: str
     distances_queue: list[Distance]
-    current_road: Distance
 
     def __init__(self, point, distances):
         self.point_id = point
@@ -16,10 +15,3 @@ class DistanceIndexElement:
         for distance in distances:
             tmp_distances.append(Distance(distance[0], distance[1]))
         return tmp_distances
-
-    def get_next_candidate_road_id(self):
-        if len(self.distances_queue) == 0:
-            self.current_road = None
-            return None
-        self.current_road = self.distances_queue.pop(0)
-        return str(self.current_road.road_id)
