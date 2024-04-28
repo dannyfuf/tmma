5aff0278f0130bec22930da49891d87e2674dd3a
Danny Fuentes <dannyfuf@gmail.com>
2024-04-27 22:14:25 -0400

    feat: add preprocesing class

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from gis.project.project import Project
 from gis.layers.normalizer.main import Normalizer
 
 from tmma.distance_index.distance_index import DistanceIndex
+from tmma.preprocesing.main import Preprocessing
 from tmma.utils import get_path
 from tmma.preprocesing.buffer_tuner import BufferTuner
 
@@ -30,12 +31,11 @@ def main():
         distance_index_path='.data/.distance_index.json'
     )
 
-    buffer_tuner = BufferTuner(distance_index)
-    buffer_size = buffer_tuner.get_buffer()
-    print('buffer size: ', buffer_size)
-
-    distance_index.remove_roads_outside_buffer(buffer_size)
-    distance_index.save_to('.data/.distance_index_filtered.json')
-
-    norm_filtered_roads = distance_index.get_layer_of_current_roads()
-    norm_filtered_roads.save_to(get_path('portageroads_cleaned.gpkg'))
+    preprocessing = Preprocessing(
+        road_layer=road_layer,
+        points_layer=points_layer,
+        distance_index=distance_index
+    )
+    preprocessing.run()
+    buffer_size = preprocessing.buffer_size
+    print(f'buffer size: {buffer_size}')
---
+++ tmma/preprocesing/main.py
@@ -0,0 +1,33 @@
+
+from gis.layers.layer.main import Layer
+from gis.project.project import Project
+from tmma.distance_index.distance_index import DistanceIndex
+from tmma.preprocesing.buffer_tuner import BufferTuner
+
+
+class Preprocessing:
+    buffer_size: float
+    filtered_roads: Layer
+
+    def __init__(
+            self,
+            road_layer: Layer,
+            points_layer: Layer,
+            distance_index: DistanceIndex
+        ):
+        self._road_layer = road_layer
+        self._points_layer = points_layer
+        self._distance_index = distance_index
+
+    def run(self):
+        buffer_tuner = BufferTuner(self._distance_index)
+        buffer_size = buffer_tuner.get_buffer()
+        self.buffer_size = buffer_size
+        print(f'buffer size: {buffer_size}')
+
+        self._distance_index.remove_roads_outside_buffer(buffer_size)
+        self._distance_index.save_to('.data/.distance_index_filtered.json')
+        norm_filtered_roads = self._distance_index.get_layer_of_current_roads()
+        norm_filtered_roads.save_to(f'.data/{norm_filtered_roads.name()}_cleaned.gpkg')
+        self.filtered_roads = norm_filtered_roads
+
