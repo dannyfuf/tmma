9ed0047ff0c3e73c8c26ca9b744d77fd96ff90af
Danny Fuentes <dannyfuf@gmail.com>
2024-05-17 01:36:39 -0400

    feat: add line methods

--- gis/geometries/lines.py
+++ gis/geometries/lines.py
@@ -1,6 +1,4 @@
-from qgis.core import (
-    QgsFeature
-)
+from qgis.core import QgsFeature
 
 class Line:
     __feature: QgsFeature = None
@@ -11,8 +9,24 @@ class Line:
     def geometry(self):
         return self.__feature.geometry()
 
+    def feature(self):
+        return self.__feature
+
     def project(self, point: QgsFeature):
-        return self.geometry().closestSegmentWithContext(point.geometry().asPoint())[1]
+        from gis import Point
+
+        shortest_line_geometry = self.geometry().shortestLine(point.geometry())
+        intersection_point = shortest_line_geometry.intersection(self.geometry())
+        feature = QgsFeature()
+        feature.setGeometry(intersection_point)
+        feature.setAttributes(point.feature().attributes())
+        return Point(feature)
 
     def id(self):
         return self.__feature['fid']
+
+    def length(self):
+        return self.geometry().length()
+    
+    def length_to(self, point):
+        return self.geometry().lineLocatePoint(point.geometry())
