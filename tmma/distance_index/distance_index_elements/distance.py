a6e5758c19f2e2960ad36068fe1ea08bc6ad3850
Danny Fuentes <dannyfuf@gmail.com>
2024-05-18 23:39:09 -0400

    chore: remove unused field

--- tmma/distance_index/distance_index_elements/distance.py
+++ tmma/distance_index/distance_index_elements/distance.py
@@ -3,10 +3,8 @@ from gis.geometries.lines import Line
 
 class Distance:
     road: Line
-    road_id: str
     distance: float
 
     def __init__(self, road, distance):
         self.road = road
-        self.road_id = road.id()
         self.distance = distance
--- tmma/distance_index/distance_index_elements/distance_index_element.py
+++ tmma/distance_index/distance_index_elements/distance_index_element.py
@@ -2,11 +2,14 @@ from gis.geometries.points import Point
 from .distance import Distance
 
 class DistanceIndexElement:
-    point_id: str
     point: Point
     distances_queue: list[Distance]
 
     def __init__(self, point, road_distances):
         self.point = point
-        self.point_id = point.id()
         self.distances_queue = road_distances
+
+    def get_closest_road(self):
+        closest_road = self.distances_queue[0]
+        self.distances_queue.pop(0)
+        return closest_road
