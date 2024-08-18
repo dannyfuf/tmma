cbc82daf5a06378e8032629fcac89249d7d76b6d
Danny Fuentes <dannyfuf@gmail.com>
2024-08-18 18:13:37 -0400

    chore: update exports

--- tmma/__init__.py
+++ tmma/__init__.py
@@ -1,3 +1,4 @@
 from .preprocesing.main import Preprocessing
-from .snapping.route_generator import RouteGenerator
+from .snapping.snap import Snap
 from .distance_index import DistanceIndex
+from .main import TMMA
