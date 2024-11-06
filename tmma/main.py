0f7187f684e7c597f196656d4b7f9b62d71315bc
Danny Fuentes <dannyfuf@gmail.com>
2024-11-06 00:58:02 -0300

    feat: update command to send last snap as parameter and call command to try to snap neighbours

--- tmma/main.py
+++ tmma/main.py
@@ -4,6 +4,7 @@ from gis import Point, Layer, Line
 from tmma import DistanceIndex, Snap
 from tmma.road_graph.main import RoadGraph
 from tmma.snapping.snap_to_road import SnapToRoad
+from tmma.snapping.try_to_snap_with_near_points import TryToSnapWithNeighbourPoints
 
 class TMMA:
     _route: list[Snap]
@@ -21,30 +22,56 @@ class TMMA:
         self._points = distance_index.points()
         self._current_point_idx = 0
         self._route = []
-        self.snap_in_current_iteration = False
+        self.point_snapped = False
 
     def run(self):
         current_point = self._get_next_point()
         while current_point:
             self.snap_in_current_iteration = False
             self._snap_to_road(current_point)
+
+            if not self.point_snapped:
+                # based on the result of TryToSnapWithNeighbourPoints the next point could be
+                # way ahead of the current point, so i need to consider this in the _get_next_point()
+                # method. Maybe the command should return the id of the point snaped
+                # FYI inside the TryToSnapWithNeighbourPoints the route is updated
+                TryToSnapWithNeighbourPoints(
+                    self._distance_index,
+                    self.distance_index,
+                    self._road_graph,
+                    self._route,
+                    self._current_point_idx,
+                    self._speed_tolerance
+                ).run()
+
             current_point = self._get_next_point()
 
     def _get_next_point(self):
         if self._current_point_idx < len(self._points):
             self._current_point_idx += 1
             return self._points[self._current_point_idx - 1]
-        else:
-            return None
 
     def _snap_to_road(self, point: Point):
-        SnapToRoad(
+        snap = SnapToRoad(
             self._distance_index,
             self._road_graph,
-            self._route,
             point,
+            self._get_last_snap(),
             self._speed_tolerance
         ).run()
+        if snap:
+            self._add_snap_to_route(snap)
+            self.point_snapped = True
+            return
+
+        self.point_snapped = False
+        return
+
+    def _get_last_snap(self):
+        return self._route[-1]
+
+    def _add_snap_to_route(self, possible_snap: Snap):
+        self._route.append(possible_snap)
 
     def build_snapped_roads_layer(self):
         roads = list(set([snap.road.feature() for snap in self._route]))
@@ -66,4 +93,3 @@ class TMMA:
             self._point_layer.fields(),
             snapped_points
         )
-
